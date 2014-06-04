03277b7284bb463e3529854bd4595fb89090e501
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-04 15:15:48 -0700

    Defined skeleton structure of functional-shared instantiation of stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var stack = {};
+  var stackMethods = {};
+  var length = 0;
+
+  stackMethods.push = function(){
+
+  };
+
+  stackMethods.pop = function(){
+
+  };
+
+  stackMethods.size = function(){
+
+  };
+  var extend = function(to, from){
+    for (var key in from){
+      to[key] = from[key];
+    }
+  };
+  extend(stack, stackMethods);
+
+  return stack;
 };
-
-var stackMethods = {};
-
-
