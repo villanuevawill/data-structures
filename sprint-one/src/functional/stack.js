1412f86d4ca0c19d20515aab5f4056ac58dbfedc
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-04 14:43:34 -0700

    Passed first three tests of stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,22 @@
 var makeStack = function(){
   var someInstance = {};
+  var length = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length]=value;
+    length++;
+
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
