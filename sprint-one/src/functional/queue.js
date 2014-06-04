8d8446eb5a620e0ce3412dd75819ba096cf5fc67
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-04 15:08:01 -0700

    Implemented functional instantiation of queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[length] = value;
+    length++;
+    return length;
   };
 
   someInstance.dequeue = function(){
+    if (length > 0) {
+      var temp = storage[0];
+      length--;
+      delete storage[0];
+      //resequence keys to go from 0...n in numeric order
+      for (var i = 1; i <= length; i++) {
+        storage[i-1] = storage[i];
+      }
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
