6715c19c701bef88f4e21f1f87470aa05a72176e
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-05 10:28:10 -0700

    Implemented prototypal inhertiance instantiation pattern of stack.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack._length = 0;
+  stack._storage = {};
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this._storage[this._length] = value;
+    this._length++;
+  },
+  pop: function(){
+    if (this._length > 0) {
+      this._length--;
+      var temp = this._storage[this._length];
+      delete this._storage[this._length];
+      return temp;
+    }
+  },
+  size: function(){
+    return this._length;
+  },
+};
 
-
+var test = makeStack(); //create a new object, with a prototype stackMethods, and native keys for length and storage
+test.pop();
