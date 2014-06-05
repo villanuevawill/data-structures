f8308b402f819ad1fb65c656d926c15b770bf9f3
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-05 11:14:31 -0700

    Implemented prototypal patterns, and stack for pseudoclassical patterns.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._storage = {};
+  this._length = 0;
 };
 
+Stack.prototype.push = function(value){
+  this._storage[this._length] = value;
+  this._length++;
+};
+Stack.prototype.pop = function(){
+  if (this._length > 0) {
+    this._length--;
+    var temp = this._storage[this._length];
+    delete this._storage[this._length];
+    return temp;
+  }
+};
+Stack.prototype.size = function(){
+  return this._length;
+};
+
+
+var stack = new Stack();
 
