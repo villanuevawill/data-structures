a09167105ff52a81b86215604f195f9dd5e1248e
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-06 13:35:22 -0700

    Implemented set function with prototypal instantiation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage[item] === undefined ? false : true;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
