fa09ad6cbf7eeddc5056ae7d809dc7aa3f0112ab
William Villanueva <wvillanu89@gmail.com>
2014-06-05 19:52:33 -0700

    Began building structure for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,14 +3,25 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(node){
+  list.addToTail = function(value){
     // list[list.tail] = value;
-    // list.tail = value;
-
-
+    // list.tail = value; 
+    var node = makeNode(value);
+    if (list.head === null){
+      list.head = node;
+    }else{
+      list.tail.next = node.value;
+    }
+    list[value] = node;
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    if (list.head === list.tail){
+      list.head = null;
+    }
+
+
   };
 
   list.contains = function(target){
@@ -28,7 +39,11 @@ var makeNode = function(value){
   return node;
 };
 
-var node1 =  makeNode(4); //{value: 4, next: null}
+var node1 =  makeNode(4);
+var list1 = makeLinkedList();
+list1.addToTail(node1);
+
+ //{value: 4, next: null}
 
 /*
  * Complexity: What is the time complexity of the above functions?
