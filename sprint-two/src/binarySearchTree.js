38aa1d782c36b2231842be451b7ebfa6bb93719c
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-07 10:48:16 -0700

    Implemented prototypal instantiation of binarySearchTree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -24,10 +24,30 @@ makeBinarySearchTree.prototype.insert = function(value){
 };
 
 makeBinarySearchTree.prototype.contains = function(target){
+  if (this.value === target) {
+    return true;
+  }
 
+  if (target < this.value){
+    if (this.left && this.left.contains(target)){
+      return true;
+    }
+  }else if (target > this.value){
+    if (this.right && this.right.contains(target)){
+      return true;
+    }
+  }
+  return false;
 };
 
 makeBinarySearchTree.prototype.depthFirstLog = function(callback){
+  callback(this.value);
+  if (this.left){
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right){
+    this.right.depthFirstLog(callback);
+  }
 
 };
 
