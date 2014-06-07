629b773c0b398a87453b85ab70db51777c978f6b
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-06 22:58:21 -0700

    Minor refactor

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -19,13 +19,16 @@ treeMethods.addChild = function(value){
 treeMethods.contains = function(target){
   if (this.value === target) {
     return true;
-  } else if (this.children.length > 0) {
+  }
+
+  if (this.children.length) {
     for (var i = 0; i < this.children.length; i++) {
       if (this.children[i].contains(target)) {
         return true;
       }
     }
   }
+
   return false;
 };
 
