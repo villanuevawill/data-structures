4c3efeb90a6230120d3b31d5207d7d42f3b3859f
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-07 10:21:34 -0700

    Created structure for implementation of binarySearchTreeSpec

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,22 @@
 var makeBinarySearchTree = function(value){
+  var tree = Object.create(makeBinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+
+};
+
+makeBinarySearchTree.prototype.insert = function(value){
+
+};
+
+makeBinarySearchTree.prototype.contains = function(target){
+
+};
+
+makeBinarySearchTree.prototype.depthFirstLog = function(callback){
+
 };
 
 
