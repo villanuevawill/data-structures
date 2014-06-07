ac741ffc4d2ad117478ebf30d5e2db333fa90985
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-07 16:40:12 -0700

    Passed first 3 test cases, implemented basic logic (not including edges yet)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,19 +1,40 @@
 var Graph = function(){
+  this._storage = {};
+  this._length = 0;
+  this._first = 0;
+};
+
+var Node = function(value){
+  this._value = value;
+  this._edges = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  if (this._length === 0) {
+    this._first = new Node(newNode);
+    this._storage[newNode] = [this._first];
+  } else if (this._length === 1) {
+    if (this._storage[newNode]) {
+      this._storage[newNode].push(new Node(newNode));
+    } else {
+      this._storage[newNode] = [new Node(newNode)];
+    }
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return !!this._storage[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this._storage[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
