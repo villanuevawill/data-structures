b764585515a6cc88a55a004abe566dc2f8454a49
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-06 22:24:57 -0700

    Completed tree function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,11 +12,21 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newNode = makeTree(value);
+  this.children.push(newNode);
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
