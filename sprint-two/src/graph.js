e4958ec85c01f95c0711f328d243c8bd7da45bb9
William Villanueva <wvillanu89@gmail.com>
2014-06-08 18:41:32 -0700

    Finished implementation of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -10,15 +10,20 @@ var Node = function(value){
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  var currentNode = new Node(newNode);
   if (this._length === 0) {
-    this._first = new Node(newNode);
-    this._storage[newNode] = [this._first];
-  } else if (this._length === 1) {
-    if (this._storage[newNode]) {
-      this._storage[newNode].push(new Node(newNode));
-    } else {
-      this._storage[newNode] = [new Node(newNode)];
-    }
+    this._first = currentNode;
+    this._storage[newNode] = currentNode;
+    this._length++
+  }else if (this._length === 1) {
+    this._storage[newNode] = currentNode;
+    currentNode._edges[this._first._value] = this._first;
+    this._first._edges[newNode] = currentNode;
+    this._length++
+  } else {
+    this._storage[newNode] = currentNode;
+    currentNode._edges[toNode] = this._storage[toNode];
+    this._storage[toNode]._edges[newNode] = currentNode;
   }
 };
 
@@ -27,17 +32,43 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
+  for (key in this._storage[node]._edges){
+    if (!Object.keys(this._storage[key].edges).length){
+      delete this._storage[key];
+    }
+    delete this._storage[key]._edges[node];
+  }
   delete this._storage[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  if (this._storage[fromNode]._edges[toNode] && 
+      this._storage[toNode]._edges[fromNode]){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this._storage[fromNode]._edges[toNode] = this._storage[toNode];
+  this._storage[toNode]._edges[fromNode] = this._storage[fromNode];
+
 
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  console.log(fromNode);
+  console.log(toNode);
+  delete this._storage[fromNode]._edges[toNode];
+  delete this._storage[toNode]._edges[fromNode];
+  if (Object.keys(this._storage[fromNode]._edges).length === 0){
+    delete this._storage[fromNode];
+  }
+  if (Object.keys(this._storage[toNode]._edges).length === 0){
+    delete this._storage[toNode];
+  }
+
 };
 
 /*
